ba6bd5f64e804740f7afa2304aeffca33868fedd
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2022-11-20 00:43:09 +0400

    Date and Time

---
+++ DateTimeConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DateTimeConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTimeModels dateModel = new DateTimeModels();
+            dateModel.CurrentDateTime();
+            dateModel.DaysBetween();
+            dateModel.LeapYear();
+            dateModel.FirstDayOfPreviousMonth();
+            dateModel.LastDayOfPreviousMonth();
+            dateModel.CurrentWeekDay();
+       
+             Console.Write("Current Tokyo Date is : ");
+             dateModel.CurrentLondonDate();
+             Console.WriteLine(" ");
+
+        }
+    }
+}
+
